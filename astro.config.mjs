6fd6ade26e98898f71af479e07a2f2d51c534680
Tomaco <twtomaco@gmail.com>
2025-05-25 13:56:45 -0400

    fix: corregido base en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomasgallas.github.io',
-  base: '/mi-portfolio/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },

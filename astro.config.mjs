b7aecacc3a2f7fa2e2ab71f65e65671e9b266a12
Tomas Gallastegui <152120469+tomasgallas@users.noreply.github.com>
2025-05-25 12:18:16 -0300

    add-base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomasgallas.github.io',
+  base: '/mi-portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
